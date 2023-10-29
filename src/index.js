0ee6aedea5b1a030bbd495814107b31a96285256
Denis <denisbesarab73@gmail.com>
2023-10-29 21:49:12 +0300

    feat: create popup, send form

--- src/index.js
+++ src/index.js
@@ -3,22 +3,37 @@
 import './index.html';
 import './index.scss'
 
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('form');
     form.addEventListener('submit', formSend);
+    
+    // var Data = Object.fromEntries(new FormData(document.querySelector('#form')).entries());
+    // console.log(Data)
 
     async function formSend(e) {
         e.preventDefault();
+        console.log(e);
 
         let error = formValidate(form);
+        // let formData = JSON.stringify(Data);
 
-        let formData = new FormData(form);
+        let formData = JSON.stringify(
+            
+            {
+                "name": e.target[0].value,
+                "email": e.target[1].value,
+                "phone": e.target[2].value,
+                "comments": e.target[3].value,
+            })
 
         if (error === 0) {
             form.classList.add('_sending');
-            let response = await fetch('sendserver.php', {
+            let response = await fetch('http://localhost:9090/api/registration', {
                 method: 'POST',
                 body: formData
+
             });
             if (response.ok) {
                 let result = await response.json();
@@ -38,11 +53,12 @@ document.addEventListener('DOMContentLoaded', function () {
     function formValidate(form) {
         let error = 0;
         let formReq = document.querySelectorAll('._req');
-        // let textarea = document.getElementById('formMessage');
+        console.log(formReq);
 
 
         for (let index = 0; index < formReq.length; index++) {
             const input = formReq[index];
+            console.log(input);
             formRemoveError(input);
 
             if (input.classList.contains('_email')) {
@@ -57,7 +73,6 @@ document.addEventListener('DOMContentLoaded', function () {
             } else {
                 if (input.value === '') {
                     formAddError(input);
-                    // formAddError(textarea);
                     error++;
                 }
             }
@@ -65,14 +80,39 @@ document.addEventListener('DOMContentLoaded', function () {
         return error;
     }
     function formAddError(input) {
-        // input.parentElement.classList.add('_error');
         input.classList.add('_error')
     }
     function formRemoveError(input) {
-        // input.parentElement.classList.remove('_error');
         input.classList.remove('_error')
     }
     function emailTest(input) {
         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
     }
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const popup = document.getElementById('popup');
+    const popupClose = popup.querySelector('.popup__close');
+    const popupBackground = popup.querySelector('.popup__background');
+    const openPopupButton = document.getElementById('openPopupButton');
+
+    function openPopup() {
+        popup.classList.add('active');
+    }
+
+    function closePopup() {
+        popup.classList.remove('active');
+    }
+
+    function closePopupOnBackgroundClick(event) {
+        if (event.target === popupBackground) {
+            closePopup();
+        }
+    }
+
+    openPopupButton.addEventListener('click', openPopup);
+    popupClose.addEventListener('click', closePopup);
+    popupBackground.addEventListener('click', closePopupOnBackgroundClick);
 });
